e6b7aa7d3124a16b23a5b66b8c37861571c234fe
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-10 15:15:40 +0800

    Add project files.

---
+++ ConvertAll.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpInDepth
+{
+    internal class ConvertAll
+    {
+        static double TakeSqaureRoot(int x)
+        {
+            return Math.Sqrt(x);
+        }
+        public static List<T> MakeList<T>(T first, T second)
+        {
+            List<T> list = new();
+            list.Add(first);
+            list.Add(second);
+            return list;
+        }
+        public static void Main()
+        {
+            List<int> integers = new();
+            List<double> doubles = new();
+            
+            integers.Add(1);
+            integers.Add(2);
+            integers.Add(3);
+            integers.Add(4);
+
+            Converter<int, double> converter = TakeSqaureRoot;
+            doubles = integers.ConvertAll<double>(converter);
+
+            foreach(double value in doubles)
+            {
+                Console.WriteLine(value);
+            }
+
+            List<string> list = MakeList<string>("Horatio", "Harper");
+
+            foreach (string x in list)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using CSharpInDepth;
+
+ConvertAll.Main();
